16d6f9a750b7263a5a0141d13310b28728d5276b
ajrunafsana <zeeralibrary2020@gmail.com>
2023-05-28 21:46:06 +0530

    pattern in zeera

--- zeera.cpp
+++ zeera.cpp
@@ -7,6 +7,10 @@ int main(){
     cout<<a+b<<endl;
     cout<<"hello world!!";
 cout<<"here some example of simple c++ code";
+for(int i=0;i<=5;i++){
+    for(int m=0;m<=i;m++){
+        cout<<i;
+    }cout<<endl;
+}
 return 0;
-
 }
