6f2bf2aebb85c96593b5610a36a94fce49b61d5a
ajrun afsana <126403607+ajrunAfsana@users.noreply.github.com>
2023-06-01 22:30:15 +0530

    Update zeera.cpp

--- zeera.cpp
+++ zeera.cpp
@@ -1,16 +1,1 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a,b;
-    a=10;
-    b=20;
-    cout<<a+b<<endl;
-    cout<<"hello world!!";
-cout<<"here some example of simple c++ code";
-for(int i=0;i<=5;i++){
-    for(int m=0;m<=i;m++){
-        cout<<i;
-    }cout<<endl;
-}
-return 0;
-}
+
